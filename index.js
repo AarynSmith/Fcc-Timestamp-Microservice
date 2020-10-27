fbd049d8bceecd1a99114d465182fce1812c1146
AarynSmith <4387096+AarynSmith@users.noreply.github.com>
2020-10-27 11:14:17 -0500

    add cors for remote test validation

--- index.js
+++ index.js
@@ -1,22 +1,27 @@
 var express = require('express');
+var cors = require('cors');
 var app = express();
-var router = express.Router();
+
+app.use(cors({optionSuccessStatus: 200}));
 
 app.use(express.static('public'));
 app.get('/', function(req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
 
-
 function isValidDate(d) {
   return d instanceof Date && !isNaN(d);
 }
 
 const timestampHandler = (req, res) => {
-  let d = new Date(Number(req.params.date))
-  if (!isValidDate(d)) d = new Date(req.params.date)
-  if (!isValidDate(d)) d = new Date()
-
+  let d
+  if (req.params.date) {
+    d = new Date(Number(req.params.date))
+    if (!isValidDate(d)) d = new Date(req.params.date)
+    if (!isValidDate(d)) {
+      return res.json({error: "Invalid Date"})
+    }
+  } else d = new Date()
   res.json({unix: d.valueOf(), utc: d.toUTCString()})
 }
 
