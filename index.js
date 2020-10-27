87bd9000cf17442d4f7fd228811aa0cce2b3f5ed
AarynSmith <4387096+AarynSmith@users.noreply.github.com>
2020-10-27 09:15:38 -0500

    complete TimestampMicroservice

--- index.js
+++ index.js
@@ -7,4 +7,19 @@ app.get('/', function(req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
 
+
+function isValidDate(d) {
+  return d instanceof Date && !isNaN(d);
+}
+
+const timestampHandler = (req, res) => {
+  let d = new Date(Number(req.params.date))
+  if (!isValidDate(d)) d = new Date(req.params.date)
+  if (!isValidDate(d)) d = new Date()
+
+  res.json({unix: d.valueOf(), utc: d.toUTCString()})
+}
+
+app.get('/api/timestamp/', timestampHandler)
+app.get('/api/timestamp/:date', timestampHandler)
 app.listen(5000);
